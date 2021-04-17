c9e8ae013cff7198b4144a3116eb65a32caff6eb
trainapplogos <tempreg2k@gmail.com>
2021-04-17 04:19:07 +0300

    Lesson 22 - commit

---
+++ src/ua/lviv/trainapplogos/L5mod/ApplicationTask1.java
@@ -0,0 +1,22 @@
+package ua.lviv.trainapplogos.L5mod;
+
+public class ApplicationTask1 {
+	public static void main(String[] args) {
+		Pet cow = () -> System.out.println("I'm cow - Moo-Moo");
+		voice(cow);
+		
+		Pet cat = () -> System.out.println("I'm cat - Meow-Meow");
+		voice(cat);
+		
+		Pet dog = () -> System.out.println("I'm dog - Bark-Bark");
+		dog.voice();
+//		voice(dog);
+	}
+	public static void voice(Pet pet) {
+		pet.voice();
+	}
+}
+
+interface Pet {
+	void voice(); 		
+}
---
+++ src/ua/lviv/trainapplogos/L5mod/ApplicationTask2.java
@@ -0,0 +1,27 @@
+package ua.lviv.trainapplogos.L5mod;
+
+public class ApplicationTask2 {
+	public static void main(String[] args) {
+		Frog frogToEat = () -> System.out.println("000");
+		Frog frogToSleep = () -> System.out.println("123");
+		Frog frogToSwim = () -> System.out.println("456");
+		Frog frogToWalk = () -> System.out.println("789");
+		
+		action(frogToEat);
+		action(frogToSleep);
+		action(frogToSwim);
+		action(frogToWalk);
+		
+	}
+	
+	public static void action(Frog frog) {
+		frog.activity();
+	}
+	
+}
+
+interface Frog extends Amphibia {}
+
+interface Amphibia {
+	void activity();
+}
